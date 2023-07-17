969e036f8b4df6ca51e876ce5e5f1eeb22997ea8
tilsong <cnsong0229@gmail.com>
2023-07-17 19:27:21 +0900

    Redis Increment를 통한 쿠폰 개수 정합성 문제 해결

---
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -0,0 +1,29 @@
+package com.example.api.service;
+
+import com.example.api.domain.Coupon;
+import com.example.api.repository.CouponCountRepository;
+import com.example.api.repository.CouponRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApplyService {
+
+    private final CouponRepository couponRepository;
+
+    private final CouponCountRepository couponCountRepository;
+
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository) {
+        this.couponRepository = couponRepository;
+        this.couponCountRepository= couponCountRepository;
+    }
+
+    public void apply(Long userId) {
+        long count = couponCountRepository.increment();
+
+        if (count > 100) {
+            return;
+        }
+
+        couponRepository.save(new Coupon(userId));
+    }
+}
---
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -0,0 +1,63 @@
+package com.example.api.service;
+
+import com.example.api.repository.CouponRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class ApplyServiceTest {
+
+    @Autowired
+    private ApplyService applyService;
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @Test
+    void 한번만응모() {
+        // given when
+        applyService.apply(1L);
+
+        long count = couponRepository.count();
+
+        // then
+        assertThat(count).isEqualTo(1);
+    }
+    
+    @Test
+    void 여러명응모() throws InterruptedException {
+        // given
+        int threadCount = 1000;
+        // 병렬 작업을 쉽게 할 수 있게 도와주는 자바 API
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        // when
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+               try {
+                   applyService.apply(userId);
+               } finally {
+                   latch.countDown();
+               }
+            });
+        }
+
+        latch.await();
+
+        long count = couponRepository.count();
+
+        // then
+        assertThat(count).isEqualTo(100);
+    }
+
+}
